3ccfc682b7175383deca6c2e396dc38659fb29cf
rkasture <rohan.kasture2011@gmail.com>
2018-11-01 17:37:14 -0400

    first commit

---
+++ src/Components/LoginPage.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import "./LoginPage.css"
+import axios from "axios";
+import {Link} from 'react-router-dom';
+
+const formStyle = {
+  paddingLeft: "300px",
+  paddingTop: "20px"
+};
+
+const welcomeStyle = {
+  color: "blue",
+  fontSize: "35px"
+}; 
+const colorStyle = {
+  color: "blue",
+};  
+
+const bgStyle = {
+  height: "100%",
+  width: "100%",
+  position: "absolute"
+};
+const divStyle = {
+  paddingTop: "40px",
+  height: "100%",
+  width: "100%",
+  backgroundColor: "#F17070",
+  display: "inline-block"
+};
+const imgStyle = {
+  height: "70%",
+  width: "75%",
+  align: "center"
+};
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+
+  handleSearch = event =>{
+    event.preventDefault();
+    window.location.assign('/mainpage');
+  };
+
+  render() {
+    
+
+    return (
+          <div className="divStyle">
+          <h2 style={{color:'blue'}}> Evaluation Software</h2>
+            <div className= "fContainer" >
+                <div className ="flexItem">UserName:</div>
+                <div className ="flexItem">
+                <input
+                    type="text"
+                    required
+                    className="username"
+                    placeholder = "IU Username"
+                    ref="username"
+                  /></div>
+               </div>
+               <div className= "fContainer" >
+                <div className ="flexItem">Password:</div>
+                <div className ="flexItem">
+                  <input
+                    type="text"
+                    required
+                    className="password"
+                    placeholder = "password"
+                    ref="userpassword"
+                  /></div>
+                </div>
+              <div className="divStyle">
+                  <button
+                    onClick = {this.handleSearch}
+                    type="submit"
+                    className="btn btn-primary mb-2 btn-color"
+                  >
+                    <u>Login</u>
+                  </button>
+                  
+              </div>
+            </div>  
+
+
+     ); 
+  }
+  }
+export default LoginPage;
---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+
+    }
+
+  render() {
+    
+
+    return (
+          <div className="abc">
+          Rohanj
+          </div>  
+
+     ); 
+  }
+  }
+export default MainPage;
