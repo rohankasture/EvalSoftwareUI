86914c4c569d1e3f71683476a2ea94568789ba93
Praneta Paithankar <ppaithan@iu.edu>
2018-11-18 22:09:33 -0500

    Added draggable tiles

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -4,83 +4,131 @@ import Detail from './Detail';
 import Dropdown from 'react-dropdown';
 import './MainPage.css';
 import axios from 'axios';
-import Draggable from 'react-draggable';
-class MainPage extends Component{
-constructor(props){
-super(props);
-this.state= {
-	data : [{rank:'1',name : 'Rohan Kasture',token : '50', adjective :'Brilliant',description :'Best'},
-	{rank:'2',name : 'Praneta Paithankar',token : '50', adjective :'Friendly',description :'Good'},
-	{rank:'3',name : 'Rocco Manzo',token : '30', adjective :'Smart',description :'Pokemon player'}],
-	selected : {},
-	options : [
-		// { value: 'one', label: 'Ok' },
-		// { value: 'two', label: 'Dependable' },
-		// {
-		//  type: 'group', name: 'Good Adjectives', items: [
-		//    { value: 'three', label: 'Brilliant' },
-		//    { value: 'four', label: 'Professional' }
-		//  ]
-		// },
-		// {
-		//  type: 'group', name: 'Bad Adjectives', items: [
-		//    { value: 'five', label: 'Unreliable' },
-		//    { value: 'six', label: 'Useless' }
-		//  ]
-		// }
-		]
-};
-}
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-componentWillMount() {
-    axios
-      .get(`https://localhost:55555/team/rkasture`,{
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          crossDomain: true
-      }
-      })
-    //   .then(res => this.setState({ posts: res.data }))
-	.then(res => //console.log(res.data.BadAjectives)) 
-		this.setState({options : res.data.BadAjectives}))
-      .catch(err => console.log(err))
-}
+
   
-// handleOnClick(id) - > will set the selected property in state to the selected id from data. eg selected = data[id]
-handleOnClick = (id) =>{
-	this.setState({selected:this.state.data[id]});
-}
-render(){
-	let rows = [];
-	for (let i=0; i<this.state.data.length; i++){
-		console.log('i == ',this.state.data[i].name);
-		rows.push(<Tile name = {this.state.data[i].name} onClick={()=>this.handleOnClick(i)} />)
+class MainPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ rank: 1, name: 'Rohan Kasture', token: '50', adjective: 'Brilliant', description: 'Best', userId: 'rkasture' },
+				{ rank: 2, name: 'Praneta Paithankar', token: '50', adjective: 'Friendly', description: 'Good', userId: 'ppaithan' },
+				{ rank: 3, name: 'Rocco Manzo', token: '30', adjective: 'Smart', description: 'Pokemon player', userId: 'rmanzo' }
+			],
+			selected: {},
+			options: [
+				// { value: 'one', label: 'Ok' },
+				// { value: 'two', label: 'Dependable' },
+				// {
+				//  type: 'group', name: 'Good Adjectives', items: [
+				//    { value: 'three', label: 'Brilliant' },
+				//    { value: 'four', label: 'Professional' }
+				//  ]
+				// },
+				// {
+				//  type: 'group', name: 'Bad Adjectives', items: [
+				//    { value: 'five', label: 'Unreliable' },
+				//    { value: 'six', label: 'Useless' }
+				//  ]
+				// }
+			]
+		};
 	}
-return (
-	<div className = "mContainer" >
-	{/* iterate through data and show tile */}
-		<div className="nameDivStyle">
-		<Draggable
-			axis="y"
-			handle=".handle"
-			defaultPosition={{x: 0, y: 0}}
-			position={null}
-			grid={[25, 25]}
-			onStart={this.handleStart}
-			onDrag={this.handleDrag}
-			onStop={this.handleStop}>
-			<div className="nameChildStyle">{rows}</div>
-		</Draggable>
-		</div>
-		<div className="detailStyle">
-			<div>
-				{/* give selected item properties to the details view */}
-			<Detail options = {this.state.options} rank = {this.state.selected.rank} name = {this.state.selected.name} token = {this.state.selected.token} adjective = {this.state.selected.adjective} description  = {this.state.selected.description}/>
+
+	componentWillMount() {
+		axios
+			.get(`https://localhost:55555/team/rkasture`, {
+				headers: {
+					"Content-Type": "application/json",
+					"Access-Control-Allow-Origin": "*",
+					crossDomain: true
+				}
+			})
+			//   .then(res => this.setState({ posts: res.data }))
+			.then(res => //console.log(res.data.BadAjectives)) 
+				this.setState({ options: res.data.BadAjectives }))
+			.catch(err => console.log(err))
+	}
+
+	// handleOnClick(id) - > will set the selected property in state to the selected id from data. eg selected = data[id]
+	handleOnClick = (id) => {
+		this.setState({ selected: this.state.data[id] });
+	}
+
+	reorder = (list, startIndex, endIndex) => {
+		const result = Array.from(list);
+		const [removed] = result.splice(startIndex, 1);
+		result.splice(endIndex, 0, removed);
+
+		
+		var mapUser = result.map((user, index) =>{ 
+			user.rank = index + 1
+			return user;
+		 });
+		
+		this.handleOnClick(startIndex);
+		return mapUser;
+	};
+
+	onDragEnd = (result) => {
+		if (!result.destination) {
+			return;
+		}
+
+		const data = this.reorder(
+			this.state.data,
+			result.source.index,
+			result.destination.index
+		);
+
+		this.setState({
+			data,
+		});
+	};
+
+	render() {
+
+		return (
+			<div className="mContainer" >
+				{/* iterate through data and show tile */}
+				<div className="nameDivStyle">
+					<DragDropContext onDragEnd={this.onDragEnd}>
+						<Droppable droppableId="droppable">
+							{
+								(provided, snapshot) => (
+								<div
+									ref={provided.innerRef}>
+									{this.state.data.map((user, index) => (
+										<Draggable key={user.userId} draggableId={user.userId} index={index}>
+											{(provided, snapshot) => (
+												<div 
+													ref={provided.innerRef}
+													{...provided.draggableProps}
+													{...provided.dragHandleProps}>
+													<Tile name={user.name} onClick={()=>this.handleOnClick(index)}/>
+												</div>
+											)}
+										</Draggable>
+									))}
+									{provided.placeholder}
+								</div>
+							)}
+						</Droppable>
+
+
+					</DragDropContext>
+
+				</div>
+				<div className="detailStyle">
+					<div>
+						{/* give selected item properties to the details view */}
+						<Detail options={this.state.options} rank={this.state.selected.rank} name={this.state.selected.name} token={this.state.selected.token} adjective={this.state.selected.adjective} description={this.state.selected.description} />
+					</div>
+				</div>
 			</div>
-		</div>
-	 </div>
-	);
-}
+		);
+	}
 }
 export default MainPage;
--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 import "./Tile.css";
 
-class Tile extends Component{
+class Tile extends Component {
 
-constructor(props){
-	super(props);
-	this.state = {
-		id : this.props.id,
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: this.props.id,
+		}
+	}
+	setRef = ref => {
+		// keep a reference to the dom ref as an instance property
+		this.ref = ref;
+		// give the dom ref to react-beautiful-dnd
+		this.props.innerRef(ref);
+	};
+
+	render() {
+		const { provided, innerRef } = this.props;
+		return (
+			<div
+				onClick={this.props.onClick} className="nameStyle">
+				<p>{this.props.name}
+					<span>{this.props.rank}</span>
+				</p>
+			</div>
+		);
 	}
 }
 
-render(){
-return (
-	<div onClick = {this.props.onClick} className="nameStyle">
-	<p>{this.props.name}
-	<span>{this.props.rank}</span>
-	</p> 
-	</div>
-	);
-}
-}
-
 export default Tile;
+
